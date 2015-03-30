c9c43318199b8064ce96c179972f0bea7122c3b4
tusbea <tusbea@gmail.com>
2015-03-30 20:37:08 +0900

    listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,18 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def create
     @article = Article.new(article_params)
     @article.save
     redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
